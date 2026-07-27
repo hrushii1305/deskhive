20838eaefa12725662462931a396e12174bbc463
hrushi1305 <hrushisaini@gmail.com>
2026-07-27 12:09:46 +0530

    Day 1: Django project setup with Organization model

---
+++ organizations/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name

c4b87adb1a8ea3647d51b45c96f3fb0d088233b5
hrushi1305 <hrushisaini@gmail.com>
2026-07-31 11:19:30 +0530

    Require JWT authentication on ticket endpoints

--- tickets/views.py
+++ tickets/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Ticket
 from .serializers import TicketSerializer
 
@@ -6,7 +7,9 @@ from .serializers import TicketSerializer
 class TicketListCreateView(generics.ListCreateAPIView):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+    permission_classes = [IsAuthenticated]
 
 class TicketDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+    permission_classes = [IsAuthenticated]
